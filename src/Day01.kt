d4257472ca5731e06095363c3f1ec480d3d0339d
Robin <robin@robinsuter.ch>
2022-12-01 10:14:56 -0500

    add day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,22 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun getSumOfCalories(input: List<String>): List<Int> = input.fold(mutableListOf(0)) { acc, line ->
+        when {
+            line.isBlank() -> acc.add(0)
+            else -> acc[acc.lastIndex] += line.toInt()
+        }
+        return@fold acc
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int = getSumOfCalories(input).max()
 
-    // test if implementation meets criteria from the description, like:
+    fun part2(input: List<String>): Int = getSumOfCalories(input).sortedDescending().take(3).sum()
+
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
+
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("part 1: ${part1(input)}")
+    println("part 2: ${part2(input)}")
 }
