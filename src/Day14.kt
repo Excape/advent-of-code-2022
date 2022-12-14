df8438de88bfff76255603bdf2494330f580d3db
Robin <robin@robinsuter.ch>
2022-12-14 13:18:18 -0500

    optimize Day14

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,76 +1,65 @@
 
 fun main() {
-    fun createWall(points: List<Point>): Set<Point> {
-        val wall = points.windowed(2).flatMap { (first, second) ->
-            (if (first.x > second.x) first.x downTo second.x else first.x..second.x).flatMap { x ->
-                (if (first.y > second.y) first.y downTo second.y else first.y..second.y).map { y ->
-                    Point(x, y)
-                }
-            }
-        }.toSet()
-        return wall
-    }
+    val sandDirections = listOf(Pair(0, 1), Pair(-1, 1), Pair(1, 1))
 
-    fun prettyPrint(walls: Set<Point>, sand: Set<Point>) {
-        (sand.minOf { it.y }..sand.maxOf { it.y }).forEach { y ->
-            (sand.minOf { it.x }..sand.maxOf { it.x }).forEach { x ->
-                val char = when (Point(x, y)) {
-                    in walls -> '#'
-                    in sand -> 'o'
-                    else -> '.'
-                }
-                print(char)
+    fun createWall(points: List<Point>) = points.windowed(2)
+        .flatMap { (first, second) ->
+        (if (first.x > second.x) first.x downTo second.x else first.x..second.x).flatMap { x ->
+            (if (first.y > second.y) first.y downTo second.y else first.y..second.y).map { y ->
+                Point(x, y)
+            }
+        }
+    }.toSet()
+
+    fun prettyPrint(grid: Map<Point, Char>) {
+        val points = grid.keys
+        (points.minOf { it.y }..points.maxOf { it.y }).forEach { y ->
+            (points.minOf { it.x }..points.maxOf { it.x }).forEach { x ->
+                print(grid[Point(x, y)] ?: ".")
             }
             print('\n')
         }
     }
 
-    val sandDirections = listOf(Pair(0, 1), Pair(-1, 1), Pair(1, 1))
-
     fun simulateSandUnit(
-        walls: Set<Point>,
-        sandPoints: Set<Point>,
+        filled: Map<Point, Char>,
         lowestWall: Int,
         path: ArrayDeque<Point>
     ): Point {
-        val filledPoints = walls union sandPoints
-
         val sandPath = generateSequence(path.last()) { current ->
             sandDirections.map { (dx, dy) -> Point(current.x + dx, current.y + dy) }
-                .firstOrNull { !filledPoints.contains(it) && it.y < lowestWall + 2 }
+                .firstOrNull { !filled.contains(it) && it.y < lowestWall + 2 }
         }.toList()
         sandPath.dropLast(1).forEach { path.addLast(it) }
         return sandPath.last()
     }
 
-    fun simulateSand(walls: Set<Point>, part1: Boolean = false): Set<Point> {
-        val sand = mutableSetOf<Point>()
+    fun simulateSand(grid: Map<Point, Char>, part1: Boolean = false): Map<Point, Char> {
+        val filled = grid.toMutableMap()
         val path = ArrayDeque(listOf(Point(500, 0)))
-        val lowestWall = walls.maxOf { it.y }
+        val lowestWall = grid.keys.maxOf { it.y }
         while (path.isNotEmpty()) {
-            val nextSand = simulateSandUnit(walls, sand, lowestWall, path)
+            val nextSand = simulateSandUnit(filled, lowestWall, path)
             if (part1 && nextSand.y > lowestWall) {
-                return sand
+                return filled
             }
-            sand.add(nextSand)
+            filled[nextSand] = 'o'
             path.removeLast()
         }
-        return sand
+        return filled
     }
 
     fun parseWalls(input: List<String>) = input.map { line ->
         line.split(" -> ")
             .map { coords -> coords.split(",") }
             .map { Point(it[0].toInt(), it[1].toInt()) }
-    }.flatMap { createWall(it) }.toSet()
+    }.flatMap { createWall(it) }.associateWith { '#' }
 
     fun execute(input: List<String>, part1: Boolean): Int {
-        val walls = parseWalls(input)
-        val sand = simulateSand(walls, part1)
-
-        prettyPrint(walls, sand)
-
-        return sand.size
+        val grid = parseWalls(input)
+        val filledGrid = simulateSand(grid, part1)
+        prettyPrint(filledGrid)
+        return filledGrid.values.count {it == 'o'}
     }
 
     fun part1(input: List<String>) = execute(input, true)
@@ -79,7 +68,6 @@ fun main() {
     val testInput = readInput("Day14_test")
     check(part1(testInput) == 24)
     check(part2(testInput) == 93)
-
 
     val input = readInput("Day14")
     val part1Answer = part1(input)
